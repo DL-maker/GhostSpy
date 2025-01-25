a38dafea7dfe7c52f33b7b1505b2c152e01ca53c
BatyaBatkovich <daifi.lucas.drobchak@protonmail.com>
2025-01-25 16:30:43 -0500

    Changement total et reorganisation des fonctions dans un ordre plus logique

--- GetUsNetworkAddress.py
+++ GetUsNetworkAddress.py
@@ -1,22 +1,11 @@
 
-import psutil
-import ipaddress
-from scapy.all import ARP, Ether, srp, conf
-from scapy.all import ARP, Ether, srp
-import socket
-import struct
-import fcntl
-import os
-import psutil
-import socket
 from getmac import get_mac_address
 import socket
-
+import ipaddress
 import netifaces
+import scapy.all as scapy
 from typing import Tuple, Optional, Dict
-
-from GetHostName import hosts
-
+import psutil
 
 def get_network_address():
     # Получаем все сетевые интерфейсы
@@ -34,24 +23,13 @@ def get_network_address():
                             break
 
                     if netmask:
-                        # Вычисляем сетевой адрес
+                        # Формируем строку типа "IP/маска" для вычисления сети
                         network = ipaddress.IPv4Network(f"{ip}/{netmask}", strict=False)
-                        return network.network_address
+
+                        # Возвращаем сетевой адрес в формате IP/маска
+                        return f"{network.network_address}/{network.prefixlen}"
 
     return None
-
-
-network_address = get_network_address()
-if network_address:
-    print(f"Network address: {network_address}")
-    network = network_address
-
-else:
-    print("No network address found.")
-
-
-
-
 def get_active_interface() -> tuple[str, str, str]:
     """
     Renvoie le nom de l'interface active, son adresse IP et son masque réseau.
@@ -90,109 +68,63 @@ def get_active_interface() -> tuple[str, str, str]:
 def get_mac():
     mac_address = get_mac_address()
     return mac_address
-# Exemple d'utilisation
-resultat = get_active_interface()
-if resultat:
-    interface, ip, masque = resultat
-    print(f"Interface active: {interface}")
-    print(f"Adresse IP: {ip}")
-    print(f"Masque réseau: {masque}")
-    print(get_mac())
-else:
-    print("Aucune interface active trouvée")
+def analyser_reseau(ip_avec_masque):
+    try:
+        # Analyser l'adresse IP avec le masque
+        reseau = ipaddress.IPv4Network(ip_avec_masque, strict=False)
+        print(f"\n[INFO] Réseau détecté: {reseau}\n")
 
+        # Obtenir la plage d'adresses IP
+        ips = [str(ip) for ip in reseau.hosts()]
 
+        # Requête ARP
+        requete_arp = scapy.ARP(pdst=ips)
+        diffusion = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+        requete_arp_diffusion = diffusion / requete_arp
 
+        # Temps d'attente minimal
+        reponses = scapy.srp(requete_arp_diffusion, timeout=0.5, verbose=False)[0]
 
+        # Affichage de l'entête
+        print(f"Appareils trouvés dans le réseau {reseau}:")
+        print("-" * 80)
+        print(f"{'IP':<20} {'MAC':<20} {'Nom d\'hôte':<30}")
+        print("-" * 80)
 
+        # Compteur pour le nombre total d'hôtes
+        compteur_hotes = 0
 
-def generate_ips_in_range(network_ip, subnet_mask):
-    ip_parts = struct.unpack('!I', socket.inet_aton(network_ip))[0]
-    mask_parts = struct.unpack('!I', socket.inet_aton(subnet_mask))[0]
+        # Affichage des résultats
+        for envoye, recu in reponses:
+            try:
+                # Tente de résoudre le nom d'hôte
+                nom_hote = socket.gethostbyaddr(recu.psrc)[0]
+            except (socket.herror, socket.gaierror):
+                # Si le nom d'hôte n'est pas trouvé, afficher une valeur par défaut
+                nom_hote = "Inconnu"
 
-    # Calculer l'adresse du réseau
-    network = ip_parts & mask_parts
+            # Affichage des informations sur chaque hôte
+            print(f"{recu.psrc:<20} {recu.hwsrc:<20} {nom_hote:<30}")
+            compteur_hotes += 1
 
-    # Calculer le nombre d'adresses IP disponibles dans le réseau
-    num_ips = 2 ** (32 - bin(mask_parts).count('1')) - 2  # exclure l'adresse réseau et l'adresse de broadcast
+        # Affichage du nombre total d'hôtes
+        print("-" * 80)
+        print(f"[INFO] Nombre total d'hôtes trouvés: {compteur_hotes}")
 
-    ips = []
-    for i in range(1, num_ips + 1):
-        ip = socket.inet_ntoa(struct.pack('!I', network + i))
-        ips.append(ip)
-
-    return ips
-
-
-# Fonction pour scanner les hôtes dans un réseau donné
-def scan_network(target_ips):
-    hosts = []
-
-    for ip in target_ips:
-        arp_request = ARP(pdst=ip)
-        broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")  # Cadre Ethernet pour la diffusion
-        arp_request_broadcast = broadcast / arp_request
-
-        # Envoyer la requête ARP et obtenir les réponses
-        answered_list = srp(arp_request_broadcast, timeout=1, verbose=False, iface="wlp0s20f3")[0]
-
-        # Ajouter les hôtes répondus à la liste
-        for element in answered_list:
-            host = {
-                "ip": element[1].psrc,
-                "mac": element[1].hwsrc
-            }
-            hosts.append(host)
-
-    return hosts
-
-
-# Fonction pour afficher les résultats
-def print_results(hosts, my_ip, my_mac):
-    print("\nRésultats du scan réseau:")
-    print("IP-Adresse\t\tAdresse MAC")
-    print("-" * 40)
-
-    # Afficher votre propre IP et MAC
-    print(f"\n{my_ip}\t\t{my_mac}")
-
-    # Afficher les résultats des autres hôtes
-    for host in hosts:
-        print(f"{host['ip']}\t\t{host['mac']}")
-
-
-def main():
-    # Définir l'interface réseau que vous utilisez (par exemple, "wlp0s20f3")
-    interface = "wlp0s20f3"
-
-    # Obtenez votre propre IP et MAC et INterface
-    result = get_active_interface()
-    interfacee, ipa, masquee = resultat
-    my_mac = get_mac()
-
-    # Spécifiez l'IP et le masque de sous-réseau (ajustez en fonction de votre configuration réseau)
-    ip = ipa # L'IP de votre machine
-    subnet_mask = masquee  # Masque de sous-réseau /28 (ex. 172.20.10.0/28)
-
-    # Générer la liste des IPs dans la plage
-    target_ips = generate_ips_in_range(ip, subnet_mask)
-
-    # Afficher les résultats du scan
-    print(f"Plage pour le scan: {target_ips[0]} - {target_ips[-1]}")
-
-    # Scanner le réseau
-    hosts = scan_network(target_ips)
-
-    # Afficher les résultats
-    print_results(hosts, ipa, my_mac)
-    # listofIPHOST = []
-    # listofIPHOST += hosts
-    # listofIPHOST += [{"ip":ipa}]
-    # for i in range (len(list(listofIPHOST))):
-    #     print(listofIPHOST[i]['ip'])
-
+    except Exception as e:
+        print(f"[ERREUR] {e}")
 if __name__ == "__main__":
-    main()
+    resultat = get_active_interface()
+    if resultat:
+        interface, ip, masque = resultat
+        print(f"votre Adreese réseau: {get_network_address()}")
+        print(f"votre Interface active: {interface}")
+        print(f"votre Adresse IP: {ip}")
+        print(f"votre Masque réseau: {masque}")
+        print(f"votre mac adresse{get_mac()}")
+        analyser_reseau(get_network_address())
+    else:
+        print("Aucune interface active trouvée")
 
 
 
