3b98661ef38d61da230d3cc8e40612a343fc15db
LD-maker <laith.dababi@oteria.fr>
2025-02-06 09:52:56 +0100

    Afficher en liste les differents interphase graphique sur linux

--- Search_linux_interface.py
+++ Search_linux_interface.py
@@ -2,17 +2,16 @@ import os
 import subprocess
 
 def list_network_interfaces():
-    # Exécuter la commande ip link show pour obtenir les interfaces réseau
+    # Execute la commande ip link show pour obtenir les interphaces réseau
     result = subprocess.run(["ip", "link", "show"], capture_output=True, text=True)
     output = result.stdout
-
-    # Extraire les noms des interfaces réseau
+    # Extraire les nom des interphaces réseau
     interfaces = []
     for line in output.splitlines():
         if ": " in line:
             parts = line.split(": ")
-            if len(parts) > 1:
+            if len(parts) > 1: #Si il y a plus qu'un interphace 
                 interface = parts[1].split("@")[0].strip()
-                if interface not in ("lo"):  # Ignorer l'interface lo (loopback)
+                if interface not in ("lo"):  # Ignoree l'interphace lo
                     interfaces.append(interface)
     return interfaces
